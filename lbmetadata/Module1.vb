6db4c5493f5177e64df36ee8afffcb5eb6bbd849
Mark A Herber II <mherberii@gmail.com>
2017-02-24 15:44:55 -0500

    Second commit

--- lbmetadata/Module1.vb
+++ lbmetadata/Module1.vb
@@ -1,7 +1,41 @@
-﻿Module Module1
+﻿
+Imports Microsoft.WindowsAPICodePack
+Imports Microsoft.VisualStudio.Shell.Interop
+Imports Shell32
+Imports System.IO
+
+
+
+
+Module Module1
+
+
 
     Sub Main()
-
+        Dim FileName As String
+        FileName = "e:\superbowls.xlsx"
+        Dim Properties As Dictionary(Of Integer, KeyValuePair(Of String, String)) = GetFileProperties(FileName)
+        For Each FileProperty As KeyValuePair(Of Integer, KeyValuePair(Of String, String)) In Properties
+            Console.WriteLine("{0}: {1}", FileProperty.Value.Key, FileProperty.Value.Value)
+        Next
+        Console.ReadKey()
     End Sub
+
+    Public Function GetFileProperties(ByVal FileName As String) As Dictionary(Of Integer, KeyValuePair(Of String, String))
+        Dim Shell As New Shell
+        Dim Folder As Folder = Shell.[NameSpace](Path.GetDirectoryName(FileName))
+        Dim File As FolderItem = Folder.ParseName(Path.GetFileName(FileName))
+        Dim Properties As New Dictionary(Of Integer, KeyValuePair(Of String, String))()
+        Dim Index As Integer
+        Dim Keys As Integer = Folder.GetDetailsOf(File, 0).Count
+        For Index = 0 To Keys - 1
+            Dim CurrentKey As String = Folder.GetDetailsOf(Nothing, Index)
+            Dim CurrentValue As String = Folder.GetDetailsOf(File, Index)
+            If CurrentValue <> "" Then
+                Properties.Add(Index, New KeyValuePair(Of String, String)(CurrentKey, CurrentValue))
+            End If
+        Next
+        Return Properties
+    End Function
 
 End Module
